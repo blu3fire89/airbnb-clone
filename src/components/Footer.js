3212bb2033994217ce82ea8436e08e8b28b9d4d9
blu3fire <blu3fire89@gmail.com>
2021-05-12 11:59:58 +0800

    Done update mobile, calendar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <p>2021 Airbnb! No rights reserved - this is a demo</p>
-            <p>Privacy . Terms . Sitemap . Company Details</p>
+            <p>Privacy - Terms - Sitemap - Company Details</p>
         </div>
     )
 }
