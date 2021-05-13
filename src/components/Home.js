bedb2187a232a697aad4455bfb2b41599ef9e592
blu3fire <blu3fire89@gmail.com>
2021-05-13 13:01:40 +0800

    Done update padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,11 +16,11 @@ function Home() {
                 title='3 Bedroom room' description='Cow venison ham, ground round pastrami jerky fatback shoulder drumstick.' />
             </div>
             <div className="home-section">
-                <Card src='https://images.unsplash.com/photo-1505576391880-b3f9d713dc4f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+                <Card src='https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
                 title='Medieval Styled House' price='300' description='Cow venison ham, ground round pastrami jerky fatback shoulder strip steak leberkas pork chislic shank drumstick.' />
-                <Card src='https://images.unsplash.com/photo-1550071403-5c048808aa00?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+                <Card src='https://images.unsplash.com/photo-1594563703937-fdc640497dcd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80'
                 title='Modern Styled House' price='200' description='Cow venison ham, ground round pastrami jerky fatback shoulder strip steak leberkas pork chislic shank drumstick.' />
-                <Card src='https://images.unsplash.com/photo-1533044309907-0fa3413da946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+                <Card src='https://images.unsplash.com/photo-1553509990-7686c6f0fae1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=812&q=80'
                 title='Open Spaced House'price='200' description='Cow venison ham, strip steak leberkas pork chislic shank drumstick.' />
             </div>
         </div>
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -23,19 +23,19 @@ function SearchPage() {
                 star={4.73} price=" 30 / night" total=" 117 total"
                 />
                 
-                <SearchResult src="https://images.unsplash.com/photo-1569377750206-a249f7b5556f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                <SearchResult src="https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"
                 title="Spacious Vander House"
                 location="Private room in center of London"
                 description="1 guest - 1 bedroom - 1 bed - 1.5 shared bathroom - Wifi - kitchen - free parking - Washing Machine" 
                 star={4.73} price=" 30 / night" total=" 117 total"
                 />
-                <SearchResult src="https://images.unsplash.com/photo-1569377750206-a249f7b5556f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                <SearchResult src="https://images.unsplash.com/photo-1597308011529-f634f4cab827?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80"
                 title="Spacious Vander House"
                 location="Private room in center of London"
                 description="1 guest - 1 bedroom - 1 bed - 1.5 shared bathroom - Wifi - kitchen - free parking - Washing Machine" 
                 star={4.73} price=" 30 / night" total=" 117 total"
                 />
-                <SearchResult src="https://images.unsplash.com/photo-1569377750206-a249f7b5556f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                <SearchResult src="https://images.unsplash.com/photo-1507038772120-7fff76f79d79?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80"
                 title="Spacious Vander House"
                 location="Private room in center of London"
                 description="1 guest - 1 bedroom - 1 bed - 1.5 shared bathroom - Wifi - kitchen - free parking - Washing Machine" 
